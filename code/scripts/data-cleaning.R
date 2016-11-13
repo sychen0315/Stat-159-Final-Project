af51bac498ee9f1c8424152ce31eee52229a2e9f
Aoyi <aoyi95@berkeley.edu>
2016-11-13 15:34:32 -0800

    data cleaning: create new data frame for major city

--- code/scripts/data-cleaning.R
+++ code/scripts/data-cleaning.R
@@ -3,3 +3,5 @@ major_cities <- read.csv("data/data-sets/original-data-set/major-cities.csv")
 
 whether_major_city <- raw_data$CITY %in% major_cities$City
 raw_data$major_city <- whether_major_city
+
+major_city <- data.frame(raw_data$UNITID, raw_data$INSTNM, raw_data$major_city)
