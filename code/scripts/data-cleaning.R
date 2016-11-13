2bbd1e0d981076d7870d06b89e6011165fa9e92d
Aoyi <aoyi95@berkeley.edu>
2016-11-13 15:03:21 -0800

    add major city

--- code/scripts/data-cleaning.R
+++ code/scripts/data-cleaning.R
@@ -1,1 +1,5 @@
+raw_data <- read.csv("data/data-sets/original-data-set/MERGED2014_15_PP.csv")
 major_cities <- read.csv("data/data-sets/original-data-set/major-cities.csv")
+
+whether_major_city <- raw_data$CITY %in% major_cities$City
+raw_data$major_city <- whether_major_city
