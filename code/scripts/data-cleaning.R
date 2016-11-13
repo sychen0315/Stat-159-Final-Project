241d6c7f84eaba335847f06be3a498a52085e6f3
sychen0315 <sychen0315@gmail.com>
2016-11-13 15:36:20 -0800

    dataclea cleaning: clean raw data by regions

--- code/scripts/data-cleaning.R
+++ code/scripts/data-cleaning.R
@@ -3,15 +3,17 @@ state_df <- read.csv("data/data-sets/original-data-set/state.csv")
 merged14_15 <- read.csv("data/data-sets/original-data-set/MERGED2014_15_PP.csv")
 
 # Divide by regions
-wests_schools <- merged14_15[merged14_15$STABBR %in% state_df$West, ]
-midwest_schools <- merged14_15[merged14_15$STABBR %in% state_df$Midwest, ]
-northeast_schools <- merged14_15[merged14_15$STABBR %in% state_df$Northeast, ]
-south_schools <- merged14_15[merged14_15$STABBR %in% state_df$South, ]
+wests_schools <-merged14_15$STABBR %in% state_df$West
+midwest_schools <- merged14_15$STABBR %in% state_df$Midwest
+northeast_schools <- merged14_15$STABBR %in% state_df$Northeast
+south_schools <- merged14_15$STABBR %in% state_df$South
 
-# Write results to new files
-write.csv(wests_schools, file = "data/data-sets/cleaned-data-set/west-schools.csv")
-write.csv(midwest_schools, file = "data/data-sets/cleaned-data-set/midwest-schools.csv")
-write.csv(northeast_schools, file = "data/data-sets/cleaned-data-set/northeast-schools.csv")
-write.csv(south_schools, file = "data/data-sets/cleaned-data-set/south-schools.csv")
+# Add more columns
+merged14_15$WEST <- as.numeric(wests_schools)
+merged14_15$MIDWEST <- as.numeric(midwest_schools)
+merged14_15$NORTHEAST <- as.numeric(northeast_schools)
+merged14_15$SOUTH <- as.numeric(south_schools)
 
+# Write result to a new csv
+write.csv(merged14_15, file = "data/data-sets/cleaned-data-set/MERGED2014_15_PP_regions.csv")
 
