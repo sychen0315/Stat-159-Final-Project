023c7941278902467b2692aaabc6aef7ce8c6876
Aoyi <aoyi95@berkeley.edu>
2016-11-15 16:58:50 -0800

    anova: add output files for anova test and regression model summary

--- code/scripts/regression.R
+++ code/scripts/regression.R
@@ -1,4 +1,16 @@
 cluster_data <- read.csv("data/data-sets/cleaned-data-set/categorized-data.csv")
+anova_output_file <- "data/data-outputs/mean-difference-anova.txt"
+summary_output_file <- 
+
+# Fit an overall regression model on all institutions
+overall_fit <- lm(STU_APPLIED ~ PCTFLOAN + C100_4 + COSTT4_A + MARJOR_CITY + MINORATIO + WEST + MIDWEST + NORTHEAST, data = cluster_data)
+save(overall_fit, file = "data/regression-data/overall-ols-model.RData")
+
+aov_result <- aov(STU_APPLIED ~ cluster, data = cluster_data)
+
+sink(file = anova_output_file)
+summary(aov_result)
+sink()
 
 category <- list("WM", "WN", "MM", "MN", "NM", "NN", "SM", "SN")
 models <- c()
@@ -12,7 +24,8 @@ for (i in category) {
   summaries <- c(summaries,list(summary(model)))
 }
 
-model <- lm(STU_APPLIED ~ PCTFLOAN + C100_4 + COSTT4_A + MINORATIO, data = cluster_data)
-anova(model)
+save(summaries, file = "data/regression-data/summary-stats-for-ols-model.RData")
+
+
 
 
