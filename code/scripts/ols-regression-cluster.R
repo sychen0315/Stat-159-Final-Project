21090c17bc649dab175627777235a2d8b4a41f4d
Aoyi <aoyi95@berkeley.edu>
2016-12-02 17:20:04 -0800

    regression: add cluster regression

--- code/scripts/ols-regression-cluster.R
+++ code/scripts/ols-regression-cluster.R
@@ -1,17 +1,17 @@
+overall_data_set_regresssion = read.csv("data/data-sets/train-test-data-set/overall-data-set-regression.csv")
+
 category <- list("WM", "WN", "MM", "MN", "NM", "NN", "SM", "SN")
 models <- c()
 summaries <- c()
 
 for (i in category) {
-  filtered_data <- cluster_data[cluster_data[,i] == 1, ]
-  model <- lm(STU_APPLIED ~ MD_EARN_WNE_P10 + PCTFLOAN + C100_4 + COSTT4_A + MINORATIO, data = filtered_data)
-  models <- c(models, list(model))
-  save(model, file = paste("data/regression-data/", i,"-ols-model.RData", sep = ""))
-  summaries <- c(summaries,list(summary(model)))
+  filtered_data <- overall_data_set_regresssion[overall_data_set_regresssion[,i] == 1, ]
+  name <- paste(i, "_model", sep = "")
+  model <- lm(ln_STU_APPLIED ~ ln_MD_EARN_WNE_P10 + ln_PCTFLOAN + ln_C100_4 + 
+           ln_COSTT4_A + MINORATIO, data = filtered_data)
+  assign(name, model)  
+  save(name, file = paste("data/regression-data/", i,"-ols-model.RData", sep = ""))
 }
 
 save(summaries, file = summary_output_file)
-
-
-
 
